d9b13baf7a0733284cc315be97ab7fbffb7d5eb2
Roman Meleshin <meleshin.r@yandex.ru>
2023-02-22 23:16:54 +0300

    apdate js script

--- src/js/script.js
+++ src/js/script.js
@@ -1,9 +1,14 @@
 const hamburger = document.querySelector('.hamburger'),
     menu = document.querySelector('.menu'),
-    closeElem = document.querySelector('.menu__close');
+    closeElem = document.querySelector('.menu__close'),
+    closeLink = document.querySelector('.menu__link');
 
 hamburger.addEventListener('click', () => {
     menu.classList.add('active');
+});
+
+closeLink.addEventListener('click' , () => {
+    menu.classList.remove('active');
 });
 
 closeElem.addEventListener('click' , () => {
